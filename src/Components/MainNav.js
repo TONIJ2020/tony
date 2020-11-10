db1ad51a7f095048363cb987d5fc62a33e2f3146
tonij2020 <69843584+naccyworld@users.noreply.github.com>
2020-11-10 15:18:28 +0100

    Update MainNav.js

--- src/Components/MainNav.js
+++ src/Components/MainNav.js
@@ -1,46 +1,19 @@
-import React from 'react'
-import logo from './Genesyslogo.png'
+import React from 'react';
+import logo from './Genesyslogo.png';
+import './NavBar.css';
+import NavToggler from './Menu/NavToggler';
+
 
 function MainNav() {
     return(
         <div className="main-navigation-div">
-            <a href="/"><img src={logo} className="genesys-logo" alt="logo" /></a>
-
-            <nav className="main-navigation-bar">
-                <ul className="main-menu-ul">
-                    <li className="main-menu-li"><a className="learnable-menu" href="/learnable">Learnable</a></li>
-                    <li className="main-menu-li"><a className="agora-menu" href="/">Agora</a></li>
-                    <li className="main-menu-li"><a className="startzone-menu" href="/">StartZone</a></li>
-                    <li className="main-menu-li"><a className="about-menu" href="/">About</a></li>
-                    <li className="main-menu-li"><a className="contact-menu" href="/">Contact</a></li>
-                </ul>
-            </nav>
-            
-
             <div>
-                <a href="/" className="devstudio-text">build a product with</a>
-
-                <a href="/"><button className="main-navigation-bar-button"><p className="main-navigation-bar-button-text">devstudio</p></button></a>
+                <a href="/"><img src={logo} className="genesys-logo" alt="logo" /></a>
             </div>
-
-            <i class="fa fa-bars main-navigation-navs__toggle" id="toggleNav" onClick={toggleMenu}></i>            
+            <NavToggler />        
         </div>
-    )
+    );
 }
 
-function toggleMenu(e) {
-    var x = document.getElementsByClassName("main-menu-li");
-    var y = document.getElementById('toggleNav')
-    var i;
-    for (i = 0; i < x.length; i++) {
-        if (x[i].style.display === "block") {
-            x[i].style.display = "none";
-            y.className = 'fa fa-bars main-navigation-navs__toggle'
-        } else {
-            x[i].style.display = "block";
-            y.className = 'fa fa-bars main-navigation-navs__toggle'
-        }
-    }
-  }
 
 export default MainNav;
